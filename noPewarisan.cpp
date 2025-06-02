f10f08163639d1d6785e2d165280e7235ac674c2
Basna Yanti <basna.yanti.ft24@mail.umy.ac.id>
2025-06-02 18:42:32 +0700

    membuat class baseClass final sekaligus membuat method virtual di dalam baseClass final

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+
+class baseClass final{
+    public:
+    virtual void perkenalan()
+    {
+        cout << "Hallo aku fungsi dari base class";
+    }
+};
+
