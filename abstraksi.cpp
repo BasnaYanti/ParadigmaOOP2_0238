b3a6b2c4d68dbcdd0ac96e13468fb67703490e0f
Basna Yanti <basna.yanti.ft24@mail.umy.ac.id>
2025-06-02 19:26:30 +0700

    last commit

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -17,3 +17,13 @@ class AbstraksiKlas
             cout << "y = " << y << endl;
         }
 };
+
+int main ()
+{
+    AbstraksiKlas ak;
+    ak.setXY("Yogyakarta", "Kampus");
+    ak.display();
+
+    return 0;
+}
+
